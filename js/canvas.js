e6e9fcdad01716cb3009cc4cf5f19608957795db
Jayson <jaysonclarkclark@gmail.com>
2019-06-04 14:51:05 -0400

    Fixed rect, added text and fill option

--- js/canvas.js
+++ js/canvas.js
@@ -1,28 +1,82 @@
 const default_json = {
-  'width': 600,
-  'height': 400,
-  'background': '#fff',
+  'width': 650,
+  'height': 500,
+  'background': {
+    'type': 'color',
+    'data': 'lightblue'
+  },
   'elements': [
     {
-      'type': 'rectangle',
-      'color': '#ff2d2d',
-      'start': [0, 0],
-      'end': [200, 300]
+      'type': 'text',
+      'content': 'Fill',
+      'color': 'black',
+      'fill': true,
+      'font': '25px Arial',
+      'position': [2, 50]
     },
     {
-      'type': 'line',
-      'start': [0, 0],
-      'end': [600, 400],
-      'color': '#315abc'
+      'type': 'rectangle',
+      'color': 'red',
+      'fill': true,
+      'start': [40, 0],
+      'end': [120, 80]
     },
     {
       'type': 'circle',
-      'color': '#ff2d2d',
-      'start': [0, 0],
-      'end': [200, 300]
+      'color': 'red',
+      'fill': true,
+      'center': [180, 40],
+      'radius': 40
+    },
+    {
+      'type': 'text',
+      'content': 'No',
+      'color': 'black',
+      'font': '25px Arial',
+      'position': [2, 140]
+    },
+    {
+      'type': 'text',
+      'content': 'Fill',
+      'color': 'black',
+      'font': '25px Arial',
+      'position': [2, 160]
+    },
+    {
+      'type': 'rectangle',
+      'color': 'blue',
+      'start': [40, 100],
+      'end': [120, 180]
+    },
+    {
+      'type': 'circle',
+      'color': 'blue',
+      'center': [180, 140],
+      'radius': 40
+    },
+    {
+      'type': 'text',
+      'content': 'Other',
+      'color': 'black',
+      'fill': true,
+      'font': '17px Arial',
+      'position': [2, 250]
+    },
+    {
+      'type': 'line',
+      'color': 'red',
+      'start': [40, 200],
+      'end': [120, 280]
+    },
+    {
+      'type': 'image',
+      'url': 'image.png',
+      'position': [140, 200]
     }
   ]
 };
+
+const optionalFill = ['rectangle', 'circle', 'text'];
 
 class Canvas {
   
@@ -59,24 +113,65 @@ class Canvas {
     
       canvas.height = json.height;
       canvas.width = json.width;
-    
-      ctx.fillStyle = json.background;
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      
+      if(json.background.type === 'color') {
+        ctx.fillStyle = json.background.data;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+      }
     
       json.elements.forEach(function(element) {
         
-        ctx.fillStyle = element.color;
-        
         if(element.type === 'line') {
+          
+          ctx.strokeStyle = element.color;
           
           ctx.moveTo(element.start[0], element.start[1]);
           ctx.lineTo(element.end[0], element.end[1]);
           
           ctx.stroke();
           
-        } else if(element.type === 'rectangle') {
+        } else if(element.type === 'image') {
           
-          ctx.fillRect(element.start[0], element.start[1], element.end[1], element.end[1])
+          let img = new Image();
+          
+          img.onload = function() {
+            ctx.drawImage(img, element.position[0], element.position[1]);
+          }
+          
+          img.src = element.url;
+          
+        } else if(optionalFill.includes(element.type)) {
+          
+          ctx.fillStyle = element.color;
+          ctx.strokeStyle = element.color;
+          
+          if(element.type === 'rectangle') {
+            
+            const s = element.start;
+            element.end[0] = element.end[0] - s[0];
+            element.end[1] = element.end[1] - s[1];
+            const e = element.end;
+            
+            if(element.fill) ctx.fillRect(s[0], s[1], e[0], e[1]);
+            ctx.strokeRect(s[0], s[1], e[0], e[1]);
+          
+          } else if(element.type === 'circle') {
+          
+            ctx.beginPath();
+          
+            ctx.arc(element.center[0], element.center[1], element.radius, 0, 2 * Math.PI);
+            
+            if(element.fill) ctx.fill();
+            ctx.stroke();
+          
+          } else if(element.type === 'text') {
+            
+            ctx.font = element.font;
+            
+            if(element.fill) ctx.fillText(element.content, element.position[0], element.position[1]);
+            ctx.strokeText(element.content, element.position[0], element.position[1])
+            
+          }
           
         }
         
